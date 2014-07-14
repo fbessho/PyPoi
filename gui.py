bcaa45e65209658361dae6373c714357dc2a9ae7
Fumihiro Bessho <iwabako@gmail.com>
2014-07-14 22:21:08 +0900

    Reverted the change to fix column width, mouse position cannot be calculated accurately due to this change.

--- gui.py
+++ gui.py
@@ -10,8 +10,6 @@ from image_managers import SourceImageManager, DestinationImageManager
 
 
 class PoissonBlendingApp(Tkinter.Tk):
-    COLUMN_WIDTH = 600
-
     def __init__(self, parent):
         Tkinter.Tk.__init__(self, parent)
         self.src_img_manager = SourceImageManager()
@@ -42,13 +40,13 @@ class PoissonBlendingApp(Tkinter.Tk):
         self.grid()
 
         # Source and Destination images
-        label_dst = Tkinter.Label(self, width=self.COLUMN_WIDTH)
+        label_dst = Tkinter.Label(self)
         label_dst.grid(row=0, column=0)
         self.dst_img_manager.set_tk_label(label_dst)
 
         Tkinter.Label(self, text="+").grid(row=0, column=1)
 
-        label_src = Tkinter.Label(self, width=self.COLUMN_WIDTH)
+        label_src = Tkinter.Label(self)
         label_src.grid(row=0, column=2)
         self.src_img_manager.set_tk_label(label_src)
 
