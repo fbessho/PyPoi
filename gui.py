51e9cc93dbc38d9512153852c3f709932c625a3b
fbessho <iwabako@gmail.com>
2014-07-14 15:38:29 +0900

    Mask area can be moved by mouse drag, open a new result window

--- gui.py
+++ gui.py
@@ -1,6 +1,8 @@
+#!/usr/bin/env python
 import Tkinter
 import PIL.Image
 import PIL.ImageTk
+from PIL import ImageChops
 import poissonblending
 import numpy as np
 
@@ -33,13 +35,13 @@ class PoissonBlendingApp(Tkinter.Tk):
         self.image_mask = PIL.Image.open('./testimages/test1_mask.png')
         self.image_src = PIL.Image.open('./testimages/test1_src.png').convert("RGB")
         self.image_masked_src = PIL.Image.blend(self.image_src, self.image_mask.convert("RGB"), 0.3)
-        print "Blending.."
-        self.blend()
+        # print "Blending.."
+        # self.blend()
 
         # Display images
         self.image_tk_masked_src = PIL.ImageTk.PhotoImage(self.image_masked_src)
         self.image_tk_dst = PIL.ImageTk.PhotoImage(self.image_dst)
-        self.image_tk_result = PIL.ImageTk.PhotoImage(self.image_result)
+        # self.image_tk_result = PIL.ImageTk.PhotoImage(self.image_result)
 
         Tkinter \
             .Label(self, image=self.image_tk_dst) \
@@ -49,17 +51,14 @@ class PoissonBlendingApp(Tkinter.Tk):
             .Label(self, text="+") \
             .grid(row=0, column=1)
 
-        Tkinter \
-            .Label(self, image=self.image_tk_masked_src) \
-            .grid(row=0, column=2)
+        self.label_src = Tkinter.Label(self, image=self.image_tk_masked_src)
+        self.label_src.grid(row=0, column=2)
+        self.label_src.bind('<Button-1>', self.on_mouse_down)
+        self.label_src.bind('<B1-Motion>', self.on_mouse_move)
 
         Tkinter \
             .Button(self, text=u'Blend', command=self.blend) \
             .grid(row=1, column=0, columnspan=3)
-
-        Tkinter \
-            .Label(self, image=self.image_tk_result) \
-            .grid(row=2, column=0, columnspan=3)
 
         self.grid_columnconfigure(0, weight=1)
         # self.resizable(True, False)
@@ -75,8 +74,28 @@ class PoissonBlendingApp(Tkinter.Tk):
         mask.flags.writeable = True
 
         blended_image = poissonblending.blend(dst, src, mask, offset=(40, -30))
-
         self.image_result = PIL.Image.fromarray(np.uint8(blended_image))
+        self.image_tk_result = PIL.ImageTk.PhotoImage(self.image_result)
+
+        result_window = Tkinter.Toplevel()
+        label = Tkinter.Label(result_window, image = self.image_tk_result)
+        label.pack()
+        result_window.title("Blended Result")
+
+    def on_mouse_down(self, event):
+        print "Mouse button down:", event.x, event.y
+        self.sx, self.sy = event.x, event.y
+
+    def on_mouse_move(self, event):
+        print "Mouse move:", event.x, event.y
+        dx = event.x - self.sx
+        dy = event.y - self.sy
+        self.image_mask = ImageChops.offset(self.image_mask, dx, dy)
+        self.image_masked_src = PIL.Image.blend(self.image_src, self.image_mask.convert("RGB"), 0.3)
+        self.image_tk_masked_src = PIL.ImageTk.PhotoImage(self.image_masked_src)
+        self.label_src.configure(image = self.image_tk_masked_src)
+
+        self.sx, self.sy = event.x, event.y
 
 
 if __name__ == "__main__":
