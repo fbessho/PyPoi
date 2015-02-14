f1357c18e07da3fa59199e137f33c61e536fac90
Fumihiro Bessho (Ben) <fumihiro.bessho@gmail.com>
2015-02-14 16:36:26 +0900

    Add info to setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
 from distutils.core import setup
 setup(name='PyPoi',
       version='0.2.0',
-      author='author',
+      author='Fumihiro Bessho (Ben)',
       author_email='fumihiro.bessho@gmail.com',
+      url='https://github.com/fbessho/PyPoi',
       packages=['pypoi', 'pypoi.testimages'],
       package_data={'pypoi': ['testimages/*.png']},
       scripts=['scripts/pypoi-gui']
