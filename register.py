e8f9e659c09fa66b056a3db4a7e6240a262efe4e
Fumihiro Bessho (Ben) <fumihiro.bessho@gmail.com>
2015-08-04 22:11:55 +0900

    Add README conversion (.md -> .rst)

---
+++ register.py
@@ -0,0 +1,13 @@
+"""Used to register PyPoi to PyPI"""
+import pandoc
+import os
+
+pandoc.core.PANDOC_PATH = '/usr/local/bin/pandoc'
+
+doc = pandoc.Document()
+doc.markdown = open('README.md').read()
+f = open('README.txt', 'w')
+f.write(doc.rst)
+f.close()
+os.system("setup.py register")
+os.remove('README.txt')
--- setup.py
+++ setup.py
@@ -1,11 +1,30 @@
-from distutils.core import setup
-setup(name='PyPoi',
-      version='0.2.0',
-      author='Fumihiro Bessho (Ben)',
-      author_email='fumihiro.bessho@gmail.com',
-      url='https://github.com/fbessho/PyPoi',
-      packages=['pypoi', 'pypoi.testimages'],
-      package_data={'pypoi': ['testimages/*.png']},
-      scripts=['scripts/pypoi-gui']
-      )
+from setuptools import setup
+import os
 
+desciption = '"Py"thon program for "Poi"sson Image Editing'
+if os.path.exists('README.txt'):
+    long_description = open('README.txt').read()
+else:
+    long_description = desciption
+
+setup(
+    name='pypoi',
+    version='0.2.0',
+    description=desciption,
+    long_description=long_description,
+    license='Apache License 2.0',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: MacOS X',
+        'Environment :: Win32 (MS Windows)',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 2.7',
+    ],
+
+    author='Fumihiro Bessho (Ben)',
+    author_email='fumihiro.bessho@gmail.com',
+    url='https://github.com/fbessho/PyPoi',
+    packages=['pypoi', 'pypoi.testimages'],
+    package_data={'pypoi': ['testimages/*.png']},
+    scripts=['scripts/pypoi']
+)
